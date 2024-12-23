06d09ef0bf1e10e4c731c312790ac5cda5e920a0
ShinYounHo <suh2963@gmail.com>
2024-12-23 15:49:28 +0900

    work6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ class  App {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
+        int lastNo = 1;
 
         while (true) {
 
@@ -31,7 +32,8 @@ class  App {
                 System.out.print("작가 : ");
                 sc.nextLine();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.println(lastNo + "번 명언이 등록되었습니다.");
+                lastNo++;
             }
         }
     }
