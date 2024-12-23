b98b4e1e46675e8892a5ae7748631e7cd8563717
ShinYounHo <suh2963@gmail.com>
2024-12-23 15:33:48 +0900

    work4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,13 +15,21 @@ class  App {
 
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명언 : ");
-        sc.nextLine();
+        while (true) {
 
-        System.out.print("작가 : ");
-        sc.nextLine();
+            System.out.print("명령) ");
+            String command = sc.nextLine();
 
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+            if (command.equals("등록")) {
+
+                System.out.print("명언 : ");
+                sc.nextLine();
+
+                System.out.print("작가 : ");
+                sc.nextLine();
+
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+        }
     }
 }
