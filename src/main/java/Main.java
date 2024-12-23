05c8c7ef93bf9c146d0a5b9991ac80fb72dfe4bd
ShinYounHo <suh2963@gmail.com>
2024-12-23 15:20:00 +0900

    work3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,7 +11,17 @@ public class Main {
 class  App {
     public void run() {
 
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
+
+        System.out.print("명언 : ");
+        sc.nextLine();
+
+        System.out.print("작가 : ");
+        sc.nextLine();
+
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료");
     }
 }
