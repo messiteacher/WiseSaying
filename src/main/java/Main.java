c450692d880f1ff1929b88a7c276785707a66d5e
ShinYounHo <suh2963@gmail.com>
2024-12-24 10:41:11 +0900

    work12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,12 +13,10 @@ class  App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("== 명언 앱 ==");
         int lastNo = 0;
-        String[] contentList = new String[3];
-        String[] authorList = new String[3];
+        WiseSaying[] wiseSayingList = new WiseSaying[3];
 
-        WiseSaying wiseSaying = new WiseSaying();
+        System.out.println("== 명언 앱 ==");
 
         while (true) {
 
@@ -36,16 +34,24 @@ class  App {
                 System.out.print("작가 : ");
                 String author = sc.nextLine();
 
+                WiseSaying wiseSaying = new WiseSaying();
+
                 wiseSaying.id = ++lastNo;
                 wiseSaying.content = content;
                 wiseSaying.author = author;
+
+                wiseSayingList[lastNo - 1] = wiseSaying;
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(lastNo));
             } else if(command.equals("목록")) {
 
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+
+                for(int i = 0; i < lastNo; i++) {
+                    WiseSaying wiseSaying = wiseSayingList[i];
+                    System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+                }
             }
         }
     }
