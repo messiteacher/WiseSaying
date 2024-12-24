d16685cda3e1f96656bb0dc06f72680fe624b6b9
ShinYounHo <suh2963@gmail.com>
2024-12-24 10:22:56 +0900

    work8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,8 @@ class  App {
 
         System.out.println("== 명언 앱 ==");
         int lastNo = 0;
+        String content = "";
+        String author = "";
 
         while (true) {
 
@@ -27,12 +29,17 @@ class  App {
             } else if (command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                sc.nextLine();
+                content = sc.nextLine();
 
                 System.out.print("작가 : ");
-                sc.nextLine();
+                author = sc.nextLine();
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastNo));
+            } else if(command.equals("목록")) {
+
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("%d / %s / %s".formatted(lastNo, author, content));
             }
         }
     }
